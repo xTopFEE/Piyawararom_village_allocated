c265d750b2b5cf007fbd7d7b492f3ea719d7a076
xTopFEE <92863970+xTopFEE@users.noreply.github.com>
2022-07-12 23:44:53 +0700

    add folder statement.php

---
+++ financial_director/nav_statement/includes/load.php
@@ -0,0 +1,53 @@
+<?php require_once "class.php";
+session_start();
+
+$user = new user;
+
+$page = 0;
+
+if (!empty($_SESSION['page'])) {
+	$page = $_SESSION['page'];
+}
+
+echo "<script>console.log($page);</script>";
+
+echo $user->load($page);
+?>
+
+<script type="text/javascript">
+	$('.del').click(function() {
+
+		Swal.fire({
+			title: 'ต้องการที่จะลบข้อมูล?',
+			showCancelButton: true,
+			confirmButtonText: 'ลบข้อมูล',
+			denyButtonText: 'ยกเลิก',
+		}).then((result) => {
+
+			if (result.isConfirmed) {
+				Swal.fire('ลบข้อมูลแล้ว!', '', 'success')
+				//
+				var admin_id = $(this).attr('admin_id');
+				$.ajax({
+					url: 'includes/delete.php',
+					type: 'POST',
+					data: {
+						admin_id: admin_id
+					},
+					success: function(data) {
+						$("#table").load("includes/load.php");
+						$("#msg").html("<p class='col-sm-3 mx-auto text-center alert alert-success'>" + data + "</p>");
+						$("#msg").slideDown("slow");
+						setTimeout(function() {
+							$("#msg").slideUp(900);
+						}, 900)
+					}
+				});
+				//
+			} else if (result.isDenied) {
+				Swal.fire('Changes are not saved', '', 'info')
+			}
+		})
+
+	});
+</script>
