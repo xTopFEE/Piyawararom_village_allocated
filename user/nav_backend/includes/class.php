53c11c1cb6dd464937d34939e158e2e5842c88c9
xTopFEE <92863970+xTopFEE@users.noreply.github.com>
2022-07-21 16:09:40 +0700

    Fix query 'house_id' in backend and debt in user

--- user/nav_backend/includes/class.php
+++ user/nav_backend/includes/class.php
@@ -97,9 +97,12 @@ class user extends db
 		$this->get_id();
 		$current_user_id = $_SESSION['username'];
 		echo "<script> console.log('current_user_id: ' + '$current_user_id') </script>";
-		
+
+		$split_user_id = explode("/", $current_user_id);
+		echo "<script> console.log('split: ' + '$split_user_id[0]/$split_user_id[1]') </script>";
+
 		// echo "<script> console.log('thismonth: ' + $thismonth) </script>";	
-		$query = "WITH added_row_number AS ( SELECT *,SUM(amount) as 'sum' , ROW_NUMBER() OVER(PARTITION BY `house_id`) AS 'row_number' FROM payment WHERE month IN('มกราคม','กุมภาพันธ์','มีนาคม','เมษายน','พฤษภาคม','มิถุนายน','กรกฎาคม','สิงหาคม','กันยายน','ตุลาคม','พฤศจิกายน','ธันวาคม') AND house_id = '124/1' GROUP BY house_id ORDER BY cast(SUBSTRING_INDEX(house_id, '/', -1)as int) ) SELECT * FROM added_row_number LIMIT 20 OFFSET $page";
+		$query = "WITH added_row_number AS ( SELECT *,SUM(amount) as 'sum' , ROW_NUMBER() OVER(PARTITION BY `house_id`) AS 'row_number' FROM payment WHERE month IN('มกราคม','กุมภาพันธ์','มีนาคม','เมษายน','พฤษภาคม','มิถุนายน','กรกฎาคม','สิงหาคม','กันยายน','ตุลาคม','พฤศจิกายน','ธันวาคม') AND house_id = '$split_user_id[0]/$split_user_id[1]' GROUP BY house_id ORDER BY cast(SUBSTRING_INDEX(house_id, '/', -1)as int) ) SELECT * FROM added_row_number LIMIT 20 OFFSET $page";
 
 		// while ($min_year <= $this_year) {
 
